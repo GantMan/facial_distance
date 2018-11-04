b936d063530d0cbf7d16875312ab76f03f52e40a
Gant Laborde <GantMan@gmail.com>
2018-11-03 20:56:52 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import * as faceapi from 'face-api.js'
 
-class App extends Component {
+export default class App extends Component {
+  state={
+    distance: 0
+  }
+
+  async componentDidMount ()  {
+    await faceapi.loadSsdMobilenetv1Model('/face_model')
+    await faceapi.loadFaceLandmarkModel('/face_model')
+    await faceapi.loadFaceRecognitionModel('/face_model')
+    this.setState({
+      classification: 'done loading model'
+    })
+
+    const nic = await faceapi.fetchImage('https://i.imgur.com/ASvFZxs.jpg')
+    const nicDescript = await faceapi.allFacesSsdMobilenetv1(nic)
+
+    const otherURL = 'https://i.imgur.com/xCzhUwe.png' //this.state.thing
+    const other = await faceapi.fetchImage(otherURL)
+
+    const otherDescript = await faceapi.allFacesSsdMobilenetv1(other)
+
+
+    const distance = faceapi.round(
+      faceapi.euclideanDistance(nicDescript[0].descriptor, otherDescript[0].descriptor)
+    )
+
+    console.log('DISTANCE', distance)
+    this.setState({distance})
+  }
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>{this.state.distance}</h2>
+    </div>
+  );
   }
 }
 
-export default App;
